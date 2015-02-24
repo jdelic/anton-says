895c2dc5e82f3f02229771c83ec451719670d680
Jonas Maurus <jmaurus@laterpay.net>
2015-02-24 12:04:07 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from distutils.command.install import INSTALL_SCHEMES
+from pip.req import parse_requirements
+
+import time
+_version = "1.0.dev%s" % int(time.time())
+_packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+
+setup(
+    name='jdelic.irc.anton.says',
+    data_files=[('', ['README.md']), ],
+    version=_version,
+    packages=_packages,
+)
